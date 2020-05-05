f007fb5975bbf3ce4ed7788b89cf8674ee1731bb
Jake Turner <jake@unity3d.com>
2020-05-05 21:48:21 +0100

    Day5 Part1 & Part2

--- AoC2017/Day05/Day05Tests.cs
+++ AoC2017/Day05/Day05Tests.cs
@@ -6,10 +6,27 @@ namespace Day05
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day05(string directions, int expected)
+        [TestCase(new string[] {
+"0",
+"3",
+"0",
+"1",
+"-3"
+            }, 5, TestName = "Steps = 5")]
+        public void Steps(string[] input, int expected)
         {
-            Assert.Fail();
+            Assert.That(Program.Steps(input), Is.EqualTo(expected));
+        }
+        [TestCase(new string[] {
+"0",
+"3",
+"0",
+"1",
+"-3"
+            }, 10, TestName = "Steps2 = 10")]
+        public void Steps2(string[] input, int expected)
+        {
+            Assert.That(Program.Steps2(input), Is.EqualTo(expected));
         }
     }
 }
