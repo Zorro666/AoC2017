cc46565ea8a2185bf2935c7260dc6866ad5d8dd0
Jake Turner <jake@unity3d.com>
2020-05-05 20:50:34 +0100

    Day3 Part1 & Part2 done

--- AoC2017/Day03/Day03.cs
+++ AoC2017/Day03/Day03.cs
@@ -25,20 +25,63 @@ How many steps are required to carry the data from the square identified in your
 
 Your puzzle input is 347991.
 
+Your puzzle answer was 480.
+
+--- Part Two ---
+
+As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1.
+Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+
+So, the first few squares' values are chosen as follows:
+
+Square 1 starts with the value 1.
+Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+Once a square is written, its value does not change. 
+Therefore, the first few squares would receive the following values:
+
+147  142  133  122   59
+304    5    4    2   57
+330   10    1    1   54
+351   11   23   25   26
+362  747  806--->   ...
+
+What is the first value written that is larger than your puzzle input?
+
 */
 
 namespace Day03
 {
     class Program
     {
+        const int MAX_DIMENSION = 512;
+        const int MIN_X = -MAX_DIMENSION;
+        const int MAX_X = MAX_DIMENSION;
+        const int MIN_Y = -MAX_DIMENSION;
+        const int MAX_Y = MAX_DIMENSION;
+        const int ARRAY_SIZE_X = (MAX_X - MIN_X) + 1;
+        const int ARRAY_SIZE_Y = (MAX_Y - MIN_Y) + 1;
+
+        readonly static long[,] sCells = new long[ARRAY_SIZE_X, ARRAY_SIZE_Y];
+        readonly static long[,] sTotalsGrid = new long[ARRAY_SIZE_X, ARRAY_SIZE_Y];
+        readonly static long[] sTotals = new long[ARRAY_SIZE_X * ARRAY_SIZE_Y];
+
         private Program(string inputFile, bool part1)
         {
             var lines = AoC.Program.ReadLines(inputFile);
+            if (lines.Length != 1)
+            {
+                throw new InvalidProgramException($"Invalid input not 1 line long {lines.Length}");
+            }
+            var input = long.Parse(lines[0]);
+
             if (part1)
             {
-                long result1 = -666;
+                var result1 = SpiralSteps(input);
                 Console.WriteLine($"Day03 : Result1 {result1}");
-                long expected = 280;
+                var expected = 480;
                 if (result1 != expected)
                 {
                     throw new InvalidProgramException($"Part1 is broken {result1} != {expected}");
@@ -46,14 +89,230 @@ namespace Day03
             }
             else
             {
-                long result2 = -123;
+                var result2 = SpiralSteps2(input);
                 Console.WriteLine($"Day03 : Result2 {result2}");
-                long expected = 1797;
+                var expected = 349975;
                 if (result2 != expected)
                 {
                     throw new InvalidProgramException($"Part2 is broken {result2} != {expected}");
                 }
             }
+        }
+
+        public static long SpiralStepsRoot(long value)
+        {
+            // Find next odd number square larger than the vaue
+            double floatSqRoot = Math.Sqrt(value);
+            long intSqRoot = (long)floatSqRoot;
+            if (intSqRoot * intSqRoot > value)
+            {
+                throw new InvalidProgramException($"Round-down Integer square root failed {intSqRoot * intSqRoot} > {value}");
+            }
+            if (intSqRoot * intSqRoot != value)
+            {
+                ++intSqRoot;
+            }
+            if (intSqRoot * intSqRoot < value)
+            {
+                throw new InvalidProgramException($"Next Integer square root failed {intSqRoot * intSqRoot} > {value}");
+            }
+            if ((intSqRoot % 2) == 0)
+            {
+                ++intSqRoot;
+            }
+            var startDx = (intSqRoot - 1) / 2;
+            var startDy = (intSqRoot - 1) / 2;
+            var dx = startDx;
+            var dy = startDy;
+            var spiralValue = intSqRoot * intSqRoot;
+            while (spiralValue > value)
+            {
+                --spiralValue;
+                // Go along the bottom edge
+                if (dy == startDy)
+                {
+                    if ((dx > -startDx) && (dx <= startDx))
+                    {
+                        --dx;
+                    }
+                    else if (dx == -startDx)
+                    {
+                        // Start to go up the left edge
+                        --dy;
+                    }
+                }
+                // Go up the left edge
+                else if (dx == -startDx)
+                {
+                    if ((dy > -startDy) && (dy <= startDy))
+                    {
+                        --dy;
+                    }
+                    else if (dy == -startDy)
+                    {
+                        // Start to go along the top edge
+                        ++dx;
+                    }
+                }
+                // Go along the top edge
+                else if (dy == -startDy)
+                {
+                    if ((dx >= -startDx) && (dx < startDx))
+                    {
+                        ++dx;
+                    }
+                    else if (dx == startDx)
+                    {
+                        // Start to go down the right edge
+                        ++dy;
+                    }
+                }
+                // Go down the right edge
+                else if (dx == startDx)
+                {
+                    if ((dy >= -startDy) && (dy < startDy))
+                    {
+                        ++dy;
+                    }
+                    else if (dy == startDy)
+                    {
+                        // Start to go along the bottom edge
+                        --dx;
+                    }
+                }
+                else
+                {
+                    throw new InvalidProgramException($"Unknown dx,dy position {dx} {dy} startDx {startDx} startDy {startDy}");
+                }
+            }
+            return Math.Abs(dx) + Math.Abs(dy);
+        }
+
+        public static long SpiralSteps(long value)
+        {
+            for (var ix = 0; ix < ARRAY_SIZE_X; ++ix)
+            {
+                for (var iy = 0; iy < ARRAY_SIZE_Y; ++iy)
+                {
+                    sCells[ix, iy] = 0;
+                    sTotalsGrid[ix, iy] = 0;
+                }
+            }
+            for (var ix = 0; ix < sTotals.Length; ++ix)
+            {
+                sTotals[ix] = 0;
+            }
+
+            var x0 = MAX_DIMENSION;
+            var y0 = MAX_DIMENSION;
+
+            sCells[x0, y0] = 1;
+            sTotalsGrid[x0, y0] = 1;
+            sTotals[0] = 1;
+            var dx = 0;
+            var dy = 0;
+            var loop = 0;
+            var spiralValue = 1;
+            var i = 1;
+            while (spiralValue < value)
+            {
+                ++spiralValue;
+                if ((dx == loop) && (dy == loop))
+                {
+                    ++loop;
+                    ++dx;
+                }
+                else
+                {
+                    // Go up the right edge
+                    if (dx == loop)
+                    {
+                        if ((dy > -loop) && (dy <= loop))
+                        {
+                            --dy;
+                        }
+                        else if (dy == -loop)
+                        {
+                            // Start to go left along the top edge
+                            --dx;
+                        }
+                    }
+                    // Go left along the top edge
+                    else if (dy == -loop)
+                    {
+                        if ((dx > -loop) && (dx <= loop))
+                        {
+                            --dx;
+                        }
+                        else if (dx == -loop)
+                        {
+                            // Start to go down the left edge
+                            ++dy;
+                        }
+                    }
+                    // Go down the left edge
+                    else if (dx == -loop)
+                    {
+                        if ((dy >= -loop) && (dy < loop))
+                        {
+                            ++dy;
+                        }
+                        else if (dy == loop)
+                        {
+                            // Start to go right along the bottom edge
+                            ++dx;
+                        }
+                    }
+                    // Go right along the bottom edge
+                    else if (dy == loop)
+                    {
+                        if ((dx >= -loop) && (dx < loop))
+                        {
+                            ++dx;
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidProgramException($"Unknown dx,dy position {dx} {dy} loop {loop}");
+                    }
+                }
+                var x = x0 + dx;
+                var y = y0 + dy;
+                sCells[x, y] = spiralValue;
+                //Console.WriteLine($"Spiral: {dx},{dy} = {spiralValue}");
+                var totalValue = 0L;
+                totalValue += sTotalsGrid[x - 1, y - 1];
+                totalValue += sTotalsGrid[x + 0, y - 1];
+                totalValue += sTotalsGrid[x + 1, y - 1];
+                totalValue += sTotalsGrid[x - 1, y + 0];
+                totalValue += sTotalsGrid[x + 1, y + 0];
+                totalValue += sTotalsGrid[x - 1, y + 1];
+                totalValue += sTotalsGrid[x + 0, y + 1];
+                totalValue += sTotalsGrid[x + 1, y + 1];
+                sTotalsGrid[x, y] = totalValue;
+                sTotals[i] = totalValue;
+                ++i;
+                //Console.WriteLine($"Total: {dx},{dy} = {totalValue}");
+            }
+            return Math.Abs(dx) + Math.Abs(dy);
+        }
+
+        public static long SpiralSteps2(long value)
+        {
+            var maxSteps = MAX_DIMENSION / 8;
+            for (var s = 32; s < maxSteps; ++s)
+            {
+                SpiralSteps(s);
+                for (var i = 0; i < sTotals.Length; ++i)
+                {
+                    var total = sTotals[i];
+                    if (total > value)
+                    {
+                        return total;
+                    }
+                }
+            }
+            throw new InvalidProgramException($"Failed to find total for {value}");
         }
 
         public static void Run()
--- AoC2017/Day03/Day03Tests.cs
+++ AoC2017/Day03/Day03Tests.cs
@@ -5,11 +5,95 @@ namespace Day03
     [TestFixture]
     public class Tests
     {
+        //  37  36  35  34  33  32  31
+        //  38  17  16  15  14  13  30
+        //  39  18   5   4   3  12  29
+        //  40  19   6   1   2  11  28
+        //  41  20   7   8   9  10  27
+        //  42  21  22  23  24  25  26
+        //  43  ->
         [Test]
-        [TestCase("", -3)]
-        public void Day03(string directions, int expected)
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 1)]
+        [TestCase(5, 2)]
+        [TestCase(6, 1)]
+        [TestCase(7, 2)]
+        [TestCase(8, 1)]
+        [TestCase(9, 2)]
+        [TestCase(10, 3)]
+        [TestCase(11, 2)]
+        [TestCase(12, 3)]
+        [TestCase(13, 4)]
+        [TestCase(14, 3)]
+        [TestCase(15, 2)]
+        [TestCase(16, 3)]
+        [TestCase(17, 4)]
+        [TestCase(18, 3)]
+        [TestCase(19, 2)]
+        [TestCase(20, 3)]
+        [TestCase(21, 4)]
+        [TestCase(22, 3)]
+        [TestCase(23, 2)]
+        [TestCase(24, 3)]
+        [TestCase(25, 4)]
+        [TestCase(26, 5)]
+        [TestCase(27, 4)]
+        [TestCase(28, 3)]
+        [TestCase(29, 4)]
+        [TestCase(30, 5)]
+        [TestCase(31, 6)]
+        [TestCase(32, 5)]
+        [TestCase(33, 4)]
+        [TestCase(34, 3)]
+        [TestCase(35, 4)]
+        [TestCase(36, 5)]
+        [TestCase(37, 6)]
+        [TestCase(38, 5)]
+        [TestCase(39, 4)]
+        [TestCase(40, 3)]
+        [TestCase(41, 4)]
+        [TestCase(42, 5)]
+        [TestCase(43, 6)]
+        [TestCase(1024, 31)]
+        public void SpiralSteps(int value, int expected)
         {
-            Assert.Fail();
+            Assert.That(Program.SpiralSteps(value), Is.EqualTo(expected));
+        }
+
+        // 147  142  133  122   59
+        // 304    5    4    2   57
+        // 330   10    1    1   54
+        // 351   11   23   25   26
+        // 362  747  806--->   ...
+        [TestCase(1, 2)]
+        [TestCase(2, 4)]
+        [TestCase(3, 4)]
+        [TestCase(4, 5)]
+        [TestCase(5, 10)]
+        [TestCase(10, 11)]
+        [TestCase(11, 23)]
+        [TestCase(20, 23)]
+        [TestCase(23, 25)]
+        [TestCase(24, 25)]
+        [TestCase(25, 26)]
+        [TestCase(26, 54)]
+        [TestCase(54, 57)]
+        [TestCase(57, 59)]
+        [TestCase(59, 122)]
+        [TestCase(122, 133)]
+        [TestCase(133, 142)]
+        [TestCase(142, 147)]
+        [TestCase(147, 304)]
+        [TestCase(304, 330)]
+        [TestCase(330, 351)]
+        [TestCase(351, 362)]
+        [TestCase(362, 747)]
+        [TestCase(747, 806)]
+        public void SpiralSteps2(int value, int expected)
+        {
+            Assert.That(Program.SpiralSteps2(value), Is.EqualTo(expected));
         }
     }
 }
