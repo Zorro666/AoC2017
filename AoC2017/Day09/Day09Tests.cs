4b1ba7109247297db584c4f1d654506dbcf67625
Jake Turner <jake@unity3d.com>
2020-05-08 18:22:37 +0100

    Day9 start

--- AoC2017/Day09/Day09Tests.cs
+++ AoC2017/Day09/Day09Tests.cs
@@ -6,10 +6,17 @@ namespace Day09
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day09(string directions, int expected)
+        [TestCase("{}", 1)]
+        [TestCase("{{{}}}", 6)]
+        [TestCase("{{},{}}", 5)]
+        [TestCase("{{{},{},{{}}}}", 16)]
+        [TestCase("{<a>,<a>,<a>,<a>}", 1)]
+        [TestCase("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9)]
+        [TestCase("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9)]
+        [TestCase("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)]
+        public void Score(string group, int expected)
         {
-            Assert.Fail();
+            Assert.That(Program.Score(group), Is.EqualTo(expected));
         }
     }
 }
