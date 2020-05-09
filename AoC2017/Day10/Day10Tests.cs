8c0603fac39ea75cc5e414c7fe0019713446ff87
Jake Turner <jake@unity3d.com>
2020-05-09 07:50:32 +0100

    Day10 Part1 & Part2 done

--- AoC2017/Day10/Day10Tests.cs
+++ AoC2017/Day10/Day10Tests.cs
@@ -6,10 +6,20 @@ namespace Day10
     public class Tests
     {
         [Test]
-        [TestCase("", -3)]
-        public void Day10(string directions, int expected)
+        [TestCase("3, 4, 1, 5", 12)]
+        public void RunSequence(string lengths, int expected)
         {
-            Assert.Fail();
+            Assert.That(Program.RunSequence(5, lengths), Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("", "a2582a3a0e66e6e86e3812dcb672a272")]
+        [TestCase("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd")]
+        [TestCase("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d")]
+        [TestCase("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
+        public void KnotHash(string input, string expectedHash)
+        {
+            Assert.That(Program.KnotHash(input), Is.EqualTo(expectedHash));
         }
     }
 }
